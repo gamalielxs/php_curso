153b4477b586bb0fb3de0ada1370e7408ac568ca
gamalielxs <gamalielcayo@gmail.com>
2024-09-30 18:46:57 -0500

    primera clae

---
+++ comentarios.php
@@ -0,0 +1,13 @@
+<?php
+
+//comentario de primera linea
+
+#comentario de primera linea
+
+/**
+ * 
+ * comentarios de multiples lineas
+ * comentarios de multiples lineas
+ * comentarios de multiples lineas
+ * 
+ */
